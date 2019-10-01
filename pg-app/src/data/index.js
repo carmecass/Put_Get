734f5113028ee52dc0a98f5825e0339543f905c2
carmecass <carmecass@gmail.com>
2019-10-01 11:11:01 +0200

    remove packjson.lock

--- pg-app/src/data/index.js
+++ pg-app/src/data/index.js
@@ -1,10 +1,10 @@
 import validate from 'pg-validate'
 import call from 'pg-call'
 
-const { REACT_APP_URL} = process.env
+const { REACT_APP_URL_LOCAL} = process.env
 
 const pgApi = {
-    __url__: REACT_APP_URL,
+    __url__: REACT_APP_URL_LOCAL,
     __timeout__: 0,
 
 /**
