e56beddae165d566d35db1e1d91bd9440d148370
carmecass <carmecass@gmail.com>
2019-10-08 18:34:00 +0200

    rev

--- pg-api/index.js
+++ pg-api/index.js
@@ -1,23 +1,38 @@
 require('dotenv').config()
 const express = require('express')
-
 const cors = require('cors')
-const {mongoose} = require('pg-data')
-const router = require('./routes')
+const { mongoose } = require('pg-data')
+const routes = require('./routes')
 
-const app = express()
 
 // const {env: {PORT, MONGO_URL : url} } = process;
 
-const {env: {PORT, MONGO_URL : url}, argv: [, , port = PORT || 8080], } = process;
+const { env: { PORT, MONGO_URL: url }, argv: [, , port = PORT || 8080], } = process;
 
-mongoose.connect(url, {useNewUrlParser: true, useFindAndModify:false, useCreateIndex: true});
+(async () => {
 
-app.use(cors())
-app.use('/api/', router)
+    try {
 
+        await mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 
+        console.log('Connection to data base')
 
-app.listen(port, () => {
-    console.log(`servidor conectado al puerto ${PORT}`)
-})
+        //express
+        const app = express()
+
+        app.use(cors())
+        app.use('/api', routes)
+
+        app.use(function (req, res, next) {
+            res.status(404).json({ error: 'Not found' })
+        })
+
+        app.listen(port, () =>
+            console.log(`servidor conectado al puerto ${PORT}`))
+
+    }
+    catch (error) {
+        console.log(error.name, error.message)
+    }
+
+})()
